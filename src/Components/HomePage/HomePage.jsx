19ce16b6342cdefcd9bac0ae164039959d9834e4
Dhruvil <thakordrvl@gmail.com>
2024-01-30 23:15:03 +0530

    just starting

---
+++ src/Components/HomePage/HomePage.jsx
@@ -0,0 +1,16 @@
+import { Grid } from '@mui/material';
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <Grid container xs={12} className='px-5 lg:px-36 justify-between'>
+
+        <Grid item xs={0} lg={2.5} className='hidden lg:block w-full relative'>
+            
+        </Grid>
+
+    </Grid>
+  );
+};
+
+export default HomePage;
